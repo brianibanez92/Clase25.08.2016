98520c14097b1650c3a901ea4c6f02e2864b9174
unknown <alumno@LAB02PC09.labs.extension>
2016-09-08 19:17:24 -0300

    cambios

---
+++ MyApplication/app/src/main/java/ibanez/brian/myapplication/Persona.java
@@ -0,0 +1,28 @@
+package ibanez.brian.myapplication;
+
+/**
+ * Created by alumno on 08/09/2016.
+ */
+public class Persona {
+
+    private String nombre;
+    private String apellido;
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+
+}
