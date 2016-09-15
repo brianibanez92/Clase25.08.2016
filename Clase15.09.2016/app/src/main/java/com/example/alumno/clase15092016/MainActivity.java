fbdb47a2e8ef181f0cdde02604ca6f5a6d25eceb
unknown <alumno@LAB02PC09.labs.extension>
2016-09-15 20:06:57 -0300

    cambios

--- Clase15.09.2016/app/src/main/java/com/example/alumno/clase15092016/MainActivity.java
+++ Clase15.09.2016/app/src/main/java/com/example/alumno/clase15092016/MainActivity.java
@@ -2,6 +2,11 @@ package com.example.alumno.clase15092016;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.io.Console;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu_layout, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (R.id.opcion1 == item.getItemId()){
+
+            Log.d("OPCION 1 ", "NOMBRE " + item.toString() + " -- ID"  + item.getItemId());
+
+        } else if (R.id.opcion2 == item.getItemId()){
+
+            Log.d("OPCION 2 ", "NOMBRE " + item.toString() + " -- ID"  + item.getItemId());
+
+        } else if (R.id.opcion3 == item.getItemId()){
+
+            Log.d("OPCION 3 ", "NOMBRE " + item.toString() + " -- ID"  + item.getItemId());
+
+        }
+
+
+
+        return super.onOptionsItemSelected(item);
     }
 }
