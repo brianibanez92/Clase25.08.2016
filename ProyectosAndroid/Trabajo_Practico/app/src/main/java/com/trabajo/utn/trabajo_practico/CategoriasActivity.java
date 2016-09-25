ccc469286ac8d838dda0908b40623208e0928555
Julian.moreno <julian.moreno@globallogic.com>
2016-09-25 17:16:35 -0300

    Se agrega layout categoria_Item y se agregan las clases CategoriaAdapter y CategoriasViewHolder

---
+++ ProyectosAndroid/Trabajo_Practico/app/src/main/java/com/trabajo/utn/trabajo_practico/CategoriasActivity.java
@@ -0,0 +1,10 @@
+package com.trabajo.utn.trabajo_practico;
+
+import android.app.Activity;
+
+/**
+ * Created by julian.moreno on 9/25/2016.
+ */
+public class CategoriasActivity extends Activity {
+
+}
--- ProyectosAndroid/Trabajo_Practico/app/src/main/java/com/trabajo/utn/trabajo_practico/MainActivity.java
+++ ProyectosAndroid/Trabajo_Practico/app/src/main/java/com/trabajo/utn/trabajo_practico/MainActivity.java
@@ -2,8 +2,18 @@ package com.trabajo.utn.trabajo_practico;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import android.support.v7.widget.LinearLayoutManager;
+
+
+import com.trabajo.utn.trabajo_practico.modelos.clases.Categoria;
+import com.trabajo.utn.trabajo_practico.vistas.adapters.CategoriaAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
