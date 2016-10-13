a195394427644880b882d82f5277e27d98186724
Julian.moreno <julian.moreno@globallogic.com>
2016-10-13 19:17:26 -0300

    Se sube modificaciones hechas hasta el momento

--- ProyectosAndroid/Trabajo_Practico/app/src/main/java/com/trabajo/utn/trabajo_practico/CategoriasActivity.java
+++ ProyectosAndroid/Trabajo_Practico/app/src/main/java/com/trabajo/utn/trabajo_practico/CategoriasActivity.java
@@ -33,10 +33,11 @@ public class CategoriasActivity extends AppCompatActivity {
     }
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
+        if(android.R.id.background == id){
+
+        }
+
 
 
 
